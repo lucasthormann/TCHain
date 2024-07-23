c8377661888dfe8e11b43cb65ad1890d32c6188c
lucasthormann <LucasThormann@protonmail.com>
2024-07-23 10:42:11 -0500

    add blockchain source file

---
+++ Blockchain.cpp
@@ -0,0 +1,16 @@
+#include "Blockchain.h"
+
+Blockchain::Blockchain() { // Blockchain constructor
+  _vChain.emplace_back(Block(0, "Genisis Block")); // emplace genisis block onto vector chain
+  _nDifficulty = 6; // default network difficulty
+}
+
+void Blockchain::AddBlock(Block bNew) {
+  bNew.sPrevHash = _GetLastBlock().GetHash(); // set previous hash variable for new block to hash of last block
+  bNew.MineBlock(_nDifficulty); // block is mined
+  _vChain.push_back(bNew); // block is added to vector chain
+}
+
+Block Blockchain::_GetLastBlock() const {
+  return _VChain.back(); // return last block in VChain vector using the vector::back method
+}
